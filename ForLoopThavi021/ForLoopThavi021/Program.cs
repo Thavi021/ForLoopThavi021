77af1eebf2627b49f004c65f152cfa6ce54c70b3
Thaveshan Naidoo <96347605+Thavi021@users.noreply.github.com>
2022-04-24 09:02:17 +0200

    Add files via upload

---
+++ ForLoopThavi021/ForLoopThavi021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoopThavi021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine(i+". Output");
+            }
+        }
+    }
+}
